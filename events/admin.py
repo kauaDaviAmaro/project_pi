9092615156d43162c1ebb2229e3cbdb27191d224
Kaua Davi <kauadaviamaro@gmail.com>
2024-10-07 19:46:24 -0300

    some addions

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'date', 'time')
+    search_fields = ('name', 'description', 'date', 'time')
+
--- products/views.py
+++ products/views.py
@@ -7,5 +7,15 @@ def home(request):
     return render(request, 'productList.html', {'products': products})
 
 def product_detail(request, product_id):
-    product = Product.objects.get(id=product_id)
+    try:
+        product = Product.objects.get(id=product_id)
+        if product is None:
+            raise Http404("Product not found")
+        product.colors = [{'value': 'preto', 'display': 'Preto'}, {'value': 'branco', 'display': 'Branco'}]
+        product.sizes = [{'value': 'p', 'display': 'Pequeno'}, {'value': 'm', 'display': 'M dio'}, {'value': 'g', 'display': 'Grande'}]
+        product.materials = [{'value': 'algod o', 'display': 'Algodão'}, {'value': 'poliester', 'display': 'Poli ster'}]
+    except Product.DoesNotExist:
+        raise Http404("Product not found")
+    except Exception as e:
+        raise Http404("Error loading product: " + str(e))
     return render(request, 'productDetail.html', {'product': product})
